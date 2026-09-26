0eebf045575817b56d84024c425459af31254b96
rohitrawat-07 <rawatrohit12386@gmail.com>
2026-09-26 15:14:37 +0530

    Time: 64 ms (40.65%), Space: 80.1 MB (34.27%) - LeetHub

--- 0648-replace-words/0648-replace-words.cpp
+++ 0648-replace-words/0648-replace-words.cpp
@@ -1,3 +1,59 @@
+class Node {
+public:
+    unordered_map<char, Node*> child;
+    bool isEnd;
+    Node() { 
+        isEnd = false;
+         }
+};
+
+class trie {
+    Node* root;
+
+public:
+    trie() {
+         root = new Node(); 
+     }
+
+    void insert(string key) {
+        Node* temp = root;
+
+        for (int i = 0; i < key.size(); i++) {
+
+            if (temp->child.count(key[i]) == 0) {
+                temp->child[key[i]] = new Node();
+            }
+
+            temp = temp->child[key[i]];
+        }
+
+        temp->isEnd = true;
+    }
+
+    void search(string key, vector<string>& ans) {
+        Node* temp = root;
+        string x = "";
+
+        for (int i = 0; i < key.size(); i++) {
+
+            if (temp->child.count(key[i]) == 0) {
+                ans.push_back(key);
+                return;
+            }
+
+            temp = temp->child[key[i]];
+            x += key[i];
+
+            if (temp->isEnd == true) {
+                ans.push_back(x);
+                return;
+            }
+        }
+
+        ans.push_back(key);
+    }
+};
+
 class Solution {
 public:
     string replaceWords(vector<string>& dictionary, string sentence) {
@@ -5,37 +61,30 @@ public:
         sentence += " ";
         string temp = "";
         for (int i = 0; i < sentence.size(); i++) {
-          if (sentence[i] == ' ') {
-          s.push_back(temp);
-          temp = "";
-          }
-          else {
-          temp += sentence[i];
-          }
-        }
-
-        unordered_map<string, int> m;
-        for (int i = 0; i < dictionary.size(); i++) {
-            m[dictionary[i]]++;
-        }
-        for (int i = 0; i < s.size(); i++) {
-            string temp = "";
-            for (int j = 0; j < s[i].size(); j++) {
-                temp += s[i][j];
-                if (m.count(temp)) {
-                    s[i] = temp;
-                    break;
-                }
+            if (sentence[i] == ' ') {
+                s.push_back(temp);
+                temp = "";
+            } else {
+                temp += sentence[i];
             }
-            
-          
         }
-        string ans = "";
+        vector<string> ans;
+
+        trie t;
+        for (int i = 0; i < dictionary.size(); i++) {
+            t.insert(dictionary[i]);
+        }
         for (int i = 0; i < s.size(); i++) {
-            ans += s[i];
-            ans += ' ';
+            t.search(s[i], ans);
         }
-        ans.pop_back();
-        return ans;
+        string result = "";
+        for (int i = 0; i < ans.size(); i++) {
+            result += ans[i];
+
+            if (i != ans.size() - 1) {
+                result += ' ';
+            }
+        }
+        return result;
     }
 };
